ba22d5caa260480f576694b845484bbf4d6bb4c6
Arjun Yadav <yadarj120@gmail.com>
2023-06-27 20:41:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", (req, res)=>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.secondName;
+    const email = req.body.email;
+    
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/632fbff343"
+    const options = {
+        method: "POST",
+        auth: "arjun:b46d033adf81b700f2e720a41c4f683f-us21"
+    }
+
+    const request = https.request(url, options, (response)=>{
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server started on port 3000");
+})
+
+// api key 
+// b46d033adf81b700f2e720a41c4f683f-us21
+
+// list id
+// 632fbff343
